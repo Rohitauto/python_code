794368626e3b5739ced5fc3117af39ff0b8739a9
Rohitauto <139200729+Rohitauto@users.noreply.github.com>
2023-07-11 15:56:06 +0530

    Add files via upload

---
+++ w3resource2.py
@@ -0,0 +1,9 @@
+#Write a Python program that calculates the area of a circle based on the radius entered by the user.
+
+#r = 1.1
+#area = 3.14*(r*r)
+#print (area)
+
+from math import pi
+r = 1.1
+print (pi*(r*r))
