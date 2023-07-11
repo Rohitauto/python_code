911f91d851d4343c50e64d06a3bcf3701299357b
Rohitauto <139200729+Rohitauto@users.noreply.github.com>
2023-07-11 16:08:27 +0530

    Add files via upload

---
+++ w3resource3.py
@@ -0,0 +1,5 @@
+#Write a Python program that accepts the user's first and last name and prints them in reverse order with a space between them.
+
+first_name = 'Hello'
+last_name = 'Everyone'
+print (last_name,first_name)
