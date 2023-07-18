80c87a282ee068e8131c1032b6488cd1a066d792
Rohitauto <139200729+Rohitauto@users.noreply.github.com>
2023-07-18 15:41:30 +0530

    Add files via upload

--- loop2.py
+++ loop2.py
@@ -1,14 +1,15 @@
 #Calculate the sum of all numbers from 1 to a given number
-s = 0
-i = int (input('Enter a value'))
-for n in range(1,i+1,1):
- s=s+n
- print(s)
-
+#s = 0
+#i = int (input('Enter a value'))
+#for n in range(1,i+1,1):
+# s=s+n
+#print(s)
 
 #Write a program to print multiplication table of a given number
 m = 1
 i = int (input('Enter a value'))
-for n in range(1,10,1):
- m=m*n
+for n in range(1,11,1):
+ m=i*n
  print(m)
+
+
